d3f1b9a65a1b006aaf017cfc3be5cabc6ff08214
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-03-30 15:04:44 +0200

    Server is Implemented

--- Payment Application/Server/server.c
+++ Payment Application/Server/server.c
@@ -12,36 +12,16 @@ ST_accountsDB_t accountsDB[DB_SIZE] = { {2000.0,	RUNNING, "8989374615436851"},
 
 ST_transaction_t transactionData[TD_SIZE] = { {0,0,0,0} };
 
-EN_transState_t	recieveTransactionData(ST_transaction_t* transData)
-{
-	uint32_t sequenceNumber = 0;
-	uint8_t cardName[25], PAN[20], expiryDate[6];
-
-
-	sequenceNumber = transData->transactionSequenceNumber;
-
-	strcpy(expiryDate, transData->cardHolderData.cardExpirationData);
-	
-	strcpy(PAN, transData->cardHolderData.primaryAcountNumber);
-	
-	strcpy(cardName, transData->cardHolderData.cardHolderName);
-	
-
-
-}
-
 EN_serverError_t isValidAccount(ST_cardData_t* cardData, ST_accountsDB_t* accountRefrence)
 {
 	uint8_t PAN[20] = {0};
 	uint8_t compare = 0, reference = 0, functionReturn = 0;
 
-	accountRefrence = accountsDB;
-
 	strcpy(PAN, cardData->primaryAcountNumber);
 	
 	for (; reference < DB_SIZE; reference++)
 	{
-		compare = strcmp(PAN, accountRefrence[reference].primaryAccountNumber);
+		compare = strcmp(PAN, accountsDB[reference].primaryAccountNumber);
 		if (!compare)
 		{
 			break;
@@ -52,19 +32,23 @@ EN_serverError_t isValidAccount(ST_cardData_t* cardData, ST_accountsDB_t* accoun
 	if (compare)
 	{
 		functionReturn = ACCOUNT_NOT_FOUND;
-		accountRefrence = NULL;
+		accountRefrence->balance = 0;
+		accountRefrence->primaryAccountNumber[0] = 0;
+		accountRefrence->state = 0;
 	}
 	else
 	{
 		functionReturn = SERVER_OK;
-		accountRefrence = accountRefrence + reference;
+		strcpy(accountRefrence->primaryAccountNumber, accountsDB[reference].primaryAccountNumber);
+		accountRefrence->balance = accountsDB[reference].balance;
+		accountRefrence->state = accountsDB[reference].state;
 	}
 
 	return functionReturn;
 
 }
 
-EN_serverError_t	isBlockedAccount(ST_accountsDB_t* accountRefrence)
+EN_serverError_t isBlockedAccount(ST_accountsDB_t* accountRefrence)
 {
 	uint8_t accountState = 0, functionReturn = 0;
 	accountState = accountRefrence->state;
@@ -104,33 +88,32 @@ EN_serverError_t isAmountAvailable(ST_terminalData_t* termData, ST_accountsDB_t*
 	return checkAmount;
 }
 
-EN_serverError_t	saveTransaction(ST_transaction_t* transData)
+EN_serverError_t saveTransaction(ST_transaction_t* transData)
 {
 	static uint8_t transactionIndex = 0;
-	uint8_t i = 0;
-	transactionData[transactionIndex].transactionSequenceNumber = transactionIndex+1;
-	
-	for (i = 0; i < 11; i++)
+	if (transactionIndex < TD_SIZE)
 	{
-		transactionData[transactionIndex].terminalData.transactionDate[i] = transData->terminalData.transactionDate[i];
+		transactionData[transactionIndex].transactionSequenceNumber = transactionIndex + 1;
+
+		strcpy(transactionData[transactionIndex].terminalData.transactionDate, transData->terminalData.transactionDate);
+
+		strcpy(transactionData[transactionIndex].cardHolderData.cardHolderName, transData->cardHolderData.cardHolderName);
+
+		strcpy(transactionData[transactionIndex].cardHolderData.primaryAcountNumber, transData->cardHolderData.primaryAcountNumber);
+
+		transactionData[transactionIndex].transState = transData->transState;
+
+		transactionData[transactionIndex].terminalData.transAmount = transData->terminalData.transAmount;
+
+		transactionIndex++;
+
+		listSavedTransactions();
 	}
-	
-	for (i = 0; transData->cardHolderData.cardHolderName[i]; i++)
+	else
 	{
-		transactionData[transactionIndex].cardHolderData.cardHolderName[i] = transData->cardHolderData.cardHolderName[i];
+		listSavedTransactions;
 	}
-	transactionData[transactionIndex].cardHolderData.cardHolderName[i] = '\0';
-
-	for (i = 0; transactionData[transactionIndex].cardHolderData.primaryAcountNumber[i]; i++)
-	{
-		transactionData[transactionIndex].cardHolderData.primaryAcountNumber[i] = transData->cardHolderData.primaryAcountNumber[i];
-	}
-	transactionData[transactionIndex].cardHolderData.primaryAcountNumber[i] = '\0';
-
-	transactionData[transactionIndex].transState = transData->transState;
-	transactionData[transactionIndex].terminalData.transAmount = transData->terminalData.transAmount;
-
-	transactionIndex++;
+	return SERVER_OK;
 }
 
 void listSavedTransactions(void)
@@ -139,7 +122,7 @@ void listSavedTransactions(void)
 	printf("TransactionSequence | TransactionDate | CardHolderName | \tPAN\t | TransactionAmount | TransactionState\n");
 	while (transactionData[transactionIndex].transactionSequenceNumber)
 	{
-		printf("%d | %s | %s | %s | %d | ", transactionData[transactionIndex].transactionSequenceNumber
+		printf("%d | %s | %s | %s | %f | ", transactionData[transactionIndex].transactionSequenceNumber
 										, transactionData[transactionIndex].terminalData.transactionDate
 										, transactionData[transactionIndex].cardHolderData.cardHolderName
 										, transactionData[transactionIndex].cardHolderData.primaryAcountNumber
@@ -148,10 +131,59 @@ void listSavedTransactions(void)
 		switch (transactionData[transactionIndex].transState)
 		{
 		case APPROVED:						printf("APPROVED\n");						break;
-		case DECLINED_INSUFFECIENT_FUN:		printf("DECLINED_INSUFFECIENT_FUN\n");		break;
+		case DECLINED_INSUFFECIENT_FUND:	printf("DECLINED_INSUFFECIENT_FUN\n");		break;
 		case DECLINED_STOLEN_CARD:			printf("DECLINED_STOLEN_CARD\n");			break;
 		case FRAUD_CARD:					printf("FRAUD_CARD\n");						break;
 		case INTERNAL_SERVER_ERROR:			printf("INTERNAL_SERVER_ERROR\n");			break;
 		}
 	}
+}
+
+EN_transState_t	recieveTransactionData(ST_transaction_t* transData)
+{
+	uint32_t sequenceNumber = 0;
+	uint8_t cardName[25], PAN[20], expiryDate[6];
+	float amount = 0;
+	uint8_t accountValidty = 0, amountAvailability = 0, accountState = 0, functionReturn = 0;
+	ST_accountsDB_t accReference;
+
+	strcpy(PAN,	transData->cardHolderData.primaryAcountNumber);
+	
+	accountValidty = isValidAccount(&transData->cardHolderData, &accReference);
+
+	accountState = isBlockedAccount(&accReference);
+
+	amountAvailability = isAmountAvailable(&transData->terminalData, &accReference);	
+
+	if (accountValidty == ACCOUNT_NOT_FOUND)
+	{
+		functionReturn = FRAUD_CARD;
+	}
+	else if (accountState == BLOCKED_ACCOUNT)
+	{
+		functionReturn = DECLINED_STOLEN_CARD;
+	}
+	else if (amountAvailability == LOW_BALANCE)
+	{
+		functionReturn = DECLINED_INSUFFECIENT_FUND;
+	}
+	else
+	{
+		functionReturn = APPROVED;
+		for (uint8_t index = 0; index < DB_SIZE; index++)
+		{	 
+			if (!strcmp(PAN, accountsDB[index].primaryAccountNumber))
+			{
+				accountsDB[index].balance -= transData->terminalData.transAmount;
+			}
+
+		}
+	}
+	
+	transData->transState = functionReturn;
+
+	saveTransaction(&transData);
+
+	return functionReturn;
+	
 }
